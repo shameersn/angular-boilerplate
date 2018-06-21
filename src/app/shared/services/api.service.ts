6936b70b5c39a896201d8609439abb7ac20e83b6
shameersn <shameer.sn@experionglobal.com>
2018-06-21 18:37:46 +0530

    added rest methods to api service

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -13,12 +12,32 @@ export class ApiService {
     this.baseUrl = environment.baseUrl;
   }
 
-  public get(url: string, queryParam?: any, loader = true): Observable<any> {
+  public get(url: string, queryParams: any = {}, loader = true): Observable<any> {
 
     return this.http
-      .get(this.getBaseUrl(url))
-      .map((res: Response) => res.json())
-      .catch((err: Response) => Observable.throw(err));
+      .get(this.getBaseUrl(url), {params: queryParams})
+      .catch((err: HttpErrorResponse) => Observable.throw(err));
+  }
+
+  public post(url: string, body: any = {}, loader = true): Observable<any> {
+
+    return this.http
+      .post(this.getBaseUrl(url), body)
+      .catch((err: HttpErrorResponse) => Observable.throw(err));
+  }
+
+  public put(url: string, body: any = {}, loader = true): Observable<any> {
+
+    return this.http
+      .put(this.getBaseUrl(url), body)
+      .catch((err: HttpErrorResponse) => Observable.throw(err));
+  }
+
+  public delete(url: string, params: any = {}, loader = true): Observable<any> {
+
+    return this.http
+      .delete(this.getBaseUrl(url), params)
+      .catch((err: HttpErrorResponse) => Observable.throw(err));
   }
 
   private getBaseUrl(url) {
--- src/app/shared/services/error.handler.ts
+++ src/app/shared/services/error.handler.ts
@@ -1,7 +1,12 @@
 import { ErrorHandler } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export class CustomErrorHandler implements ErrorHandler {
-  handleError(err) {
-    console.log('err', err);
+  handleError(err: HttpErrorResponse | any) {
+    if (err instanceof HttpErrorResponse) {
+      console.log('http err', err);
+    } else {
+      console.error('err', err);
+    }
   }
 }
